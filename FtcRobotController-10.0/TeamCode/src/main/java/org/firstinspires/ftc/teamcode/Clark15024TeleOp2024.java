242072d03a4ba28aef4d1fe59b108e0a9af2c483
ryanMathram <168239412+ryanMathram@users.noreply.github.com>
2024-09-26 00:08:10 -0500

    Basic TeleOp
    Initiated Hardware Map with runOpMode and the while loop(nothing in it yet)

---
+++ FtcRobotController-10.0/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Clark15024TeleOp2024.java
@@ -0,0 +1,39 @@
+package org.firstinspires.ftc.teamcode;
+//Imports
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+//Used to name this specific TeleOP to show in the driver hub
+@TeleOp(name = "Clark15024TeleOp2024")
+//Base TeleOp class which currently has the Hardware map Initialized and the running Op mode
+//extends LinearOpMode - used as a parent class of this child class, meaning you can use all the functions from the parent class in this child class
+public class Clark15024TeleOp2024 extends LinearOpMode {
+    //Initialized Hardware map instance variable assigned to "robot"
+    Clark15024HWMap robot = new Clark15024HWMap();
+
+    //@Override - Used to rewrite the runOpMode function which is in the LinearOpMode class
+    //runOpMode - runs when the button before the start button is pressed
+    @Override
+    public void runOpMode(){
+        //Initiates the Map function, assigning items to instance variables in the hardware map
+        robot.Map(hardwareMap);
+        //Sets the mode all motors for driving to reset the counter for the encode and to run without using the encoders(encoders will not pick up change)
+        robot.driveLeftBack.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        robot.driveLeftBack.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        robot.driveLeftFront.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        robot.driveLeftFront.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        robot.driveRightBack.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        robot.driveRightBack.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        robot.driveRightFront.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        robot.driveRightFront.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+
+        //Waits for the button to start on the driver hub to be pressed
+        waitForStart();
+
+        //while loop starts once the start button is pressed
+        while(opModeIsActive()){}
+
+    }
+
+}
