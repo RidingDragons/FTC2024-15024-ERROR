7a5c6fc0b857e4724efcf5c181b3d20cd0ffe8b0
Mihir Ghawghawe <Mihirg.301@gmail.com>
2024-09-27 13:13:45 -0500

    Upgraded Gradle Plugin
    Added //TODO comments and reviewed intial hardware map and tele-op base files.

--- FtcRobotController-10.0/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Clark15024HWMap.java
+++ FtcRobotController-10.0/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Clark15024HWMap.java
@@ -22,13 +22,15 @@ public class Clark15024HWMap {
     public void Map(HardwareMap HWM){
         //Assigns values to instance variables using the parameter which the .get functions to pull the information from the control hub
         hM = HWM;
-        driveLeftBack = HWM.get(DcMotor.class, "driveLeftBack");
+        driveLeftBack = HWM.get(DcMotor.class, "driveLeftBack"); //TODO why is there a string with the "device name"? (add a comment so people know that it is linked to whatever is set in the config file on the robot)
         driveLeftFront = HWM.get(DcMotor.class, "driveLeftFront");
         driveRightFront = HWM.get(DcMotor.class, "driveRightFront");
         driveRightBack = HWM.get(DcMotor.class, "driveRightBack");
 
         //Using functions from the DcMotor class, this changes the direction of the motor, sets the power to 0, and makes the runmode to run without encoder as the robot is not moving
         //The direction of the some variables are different because they are needed to offset each other to move
+        //TODO What I usually do to be more specific I will comment the first section of repeated code with what each line does so that they rest are self explanatory.
+        //TODO For the example below, the first 3 lines what have comments next to them explaining the specific line and then the 3 other blocks don't need anything next to them as you can just look up to the first block of three lines to figure out what is going on.
         driveLeftBack.setDirection(DcMotorSimple.Direction.FORWARD);
         driveLeftBack.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         driveLeftBack.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
