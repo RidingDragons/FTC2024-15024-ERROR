87402fb1d567ef5b06fd2e1b3f5ce963428e7fa4
Ms. Mills <danielle.mills@nisd.net>
2024-10-10 14:47:56 -0500

    added telemetry

--- FtcRobotController-10.0/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Clark15024TeleOp2024.java
+++ FtcRobotController-10.0/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Clark15024TeleOp2024.java
@@ -3,6 +3,7 @@ package org.firstinspires.ftc.teamcode;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import org.firstinspires.ftc.robotcontroller.external.samples.SensorSparkFunOTOS;
 
 //Used to name this specific TeleOP to show in the driver hub
 @TeleOp(name = "Clark15024TeleOp2024")
@@ -18,6 +19,9 @@ public class Clark15024TeleOp2024 extends LinearOpMode {
     public void runOpMode(){
         //Initiates the Map function, assigning items to instance variables in the hardware map
         robot.Map(hardwareMap);
+
+        telemetry.addData("Say", "Starting");
+        telemetry.update();
         //Sets the mode all motors for driving to reset the counter for the encode and to run without using the encoders(encoders will not pick up change)
         robot.driveLeftBack.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
         robot.driveLeftBack.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
