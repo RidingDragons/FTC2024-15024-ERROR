bfca8e009ca19661809fd6902b170a2c551d4539
ryanMathram <168239412+ryanMathram@users.noreply.github.com>
2024-10-12 08:30:14 -0500

    Added Base Autonomous + Telemetry

---
+++ FtcRobotController-10.0/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Clark15024Auto.java
@@ -0,0 +1,71 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.hardware.sparkfun.SparkFunOTOS;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+@Autonomous (name = "Clark2024Auto")
+public class Clark15024Auto extends LinearOpMode {
+    Clark15024HWMap robot = new Clark15024HWMap();
+    ElapsedTime time = new ElapsedTime();
+
+
+    @Override
+    public void runOpMode(){
+        telemetry.addData("Time", time.time());
+        telemetry.addData("Postition", robot.odom.telemetry);
+    }
+
+    public void moveForward(double p, int t){
+        robot.driveLeftBack.setPower(-p);
+        robot.driveRightFront.setPower(-p);
+        robot.driveLeftFront.setPower(-p);
+        robot.driveRightBack.setPower(-p);
+        sleep(t);
+
+        robot.driveLeftBack.setPower(0);
+        robot.driveRightFront.setPower(0);
+        robot.driveLeftFront.setPower(0);
+        robot.driveRightBack.setPower(0);
+    }
+
+    public void moveBack(double p, int t){
+        robot.driveLeftBack.setPower(p);
+        robot.driveRightFront.setPower(p);
+        robot.driveLeftFront.setPower(p);
+        robot.driveRightBack.setPower(p);
+        sleep(t);
+
+        robot.driveLeftBack.setPower(0);
+        robot.driveLeftFront.setPower(0);
+        robot.driveRightBack.setPower(0);
+        robot.driveRightFront.setPower(0);
+    }
+
+    public void moveLeft(double p, int t){
+        robot.driveLeftBack.setPower(-p);
+        robot.driveRightFront.setPower(-p);
+        robot.driveLeftFront.setPower(p);
+        robot.driveRightBack.setPower(p);
+        sleep(t);
+
+        robot.driveLeftBack.setPower(0);
+        robot.driveRightFront.setPower(0);
+        robot.driveLeftFront.setPower(0);
+        robot.driveRightBack.setPower(0);
+    }
+
+    public void moveRight(double p, int t){
+        robot.driveLeftBack.setPower(-p);
+        robot.driveRightFront.setPower(-p);
+        robot.driveLeftFront.setPower(p);
+        robot.driveRightBack.setPower(p);
+        sleep(t);
+
+        robot.driveLeftBack.setPower(0);
+        robot.driveRightFront.setPower(0);
+        robot.driveLeftFront.setPower(0);
+        robot.driveRightBack.setPower(0);
+    }
+}
